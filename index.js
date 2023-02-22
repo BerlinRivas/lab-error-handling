632c8b634c7624e671417fdeeb145628298acd5a
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-02-21 20:48:21 -0500

    You are a wizard Harry

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if(cart.length === 0) {
+    throw 'error'
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -37,7 +40,22 @@ function getCartTotal(cart) {
 */
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
+  if(products.length === 0) {
+    throw 'error'
+  }
+  if(max === 0) {
+    throw 'error'
+  }
+  if(min < 0 || max < 0) {
+    throw 'error'
+  }
+  if(typeof min !== 'number' || typeof max !== 'number') {
+    throw 'error'
+  }
   for (let product of products) {
+    if(product.priceInCents === undefined) {
+      throw 'error'
+    } 
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +67,17 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
+
+try {
   const filteredProducts = filterProductsByPriceRange(products, min, max);
   const total = getCartTotal(filteredProducts);
-
-  return total;
+  
+  return total
+  } 
+  
+   catch(error) {
+    return 0
+   }
 }
 
 module.exports = {
